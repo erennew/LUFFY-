b5268171de34670fc9b6907daa50bdd688e52992
erennew <erenyeagermikasa84@gmail.com>
2025-04-24 16:34:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
+import asyncio
 
 from config import (
     API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS,
